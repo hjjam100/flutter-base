0247d0f97fa79cccb4b640e913e144af73bae9d8
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-02-11 18:30:29 +0900

    init: flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 앱 구동 시 실행되는 함수
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
